2b5d06b02b3003f7c2126ec3cb8f3895baec6188
Vladyslav <vladnazarchuk406@gmail.com>
2025-03-30 01:06:28 +0100

    feat: add Navbar component, configure sidebar, and implement Hero with animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='overflow-x-hidden '>
+      <Hero />
+    </div>
   )
 }
 
